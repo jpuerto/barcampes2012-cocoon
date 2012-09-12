747970eb316739ef3fd5c99d2e67f84374870068
Javier Puerto <javier.puerto@becompany.ch>
2012-09-12 23:08:36 +0200

    Create base projects

---
+++ barcamp-java/src/main/java/es/codebusters/App.java
@@ -0,0 +1,13 @@
+package es.codebusters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
